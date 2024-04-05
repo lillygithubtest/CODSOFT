a5dc19078f424bca174394391d8379ced7be3a59
lillygithubtest <lillymeena23@gmail.com>
2024-04-05 17:09:21 +0530

    feet:calculator

--- calc.py
+++ calc.py
@@ -1,6 +1,33 @@
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
-    print("hellow world")
+print("Simple calculator")
 
+print("operations")
+print("1-Addition")
+print("2-Subtration")
+print("3-Multiplication")
+print("4-Division")
+print("5-Power")
+print("6-Exit")
+
+opp=input("Choose Operation(1/2/3/4/5/6):")
+if(opp in [1,2,3,4,5]):
+    x=float(input("First number"))
+    y=float(input("Seccond number = "))
+
+    if(opp==1):
+        print(x+y)
+    elif(opp==2): 
+        print(x-y)
+    elif(opp==3):
+        print(x*y) 
+    elif(opp==4):
+        if y==0:
+            print("Error! Division by zero")
+        else :
+            print((x/y)) 
+    elif(opp==5):
+        print(x**y) 
+elif(opp==6):
+     print("Exiting the calculator")   
+else:
+    print("Invalid input.Enter a valid choice(1/2/3/4/5/6): ")    
+            
