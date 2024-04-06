f60e104ebf9c7111d01fc8fa4de64ed8bccd5dff
lillygithubtest <lillymeena23@gmail.com>
2024-04-06 15:10:44 +0530

    chore:try to add result in output

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
-print("Simple calculator")
+print("Simple calculator\n")
 
-print("operations")
+print("operations:")
 print("1-Addition")
 print("2-Subtration")
 print("3-Multiplication")
@@ -10,24 +10,24 @@ print("6-Exit")
 
 opp=input("Choose Operation(1/2/3/4/5/6):")
 if(opp in [1,2,3,4,5]):
-    x=float(input("First number"))
+    x=float(input("First number = "))
     y=float(input("Seccond number = "))
 
     if(opp==1):
         print(x+y)
-    elif(opp==2): 
-        print(x-y)
+    elif(opp==2):
+       print(x-y)
     elif(opp==3):
-        print(x*y) 
+        print(x*y)
     elif(opp==4):
         if y==0:
             print("Error! Division by zero")
         else :
-            print((x/y)) 
+         print(x/y) 
     elif(opp==5):
         print(x**y) 
 elif(opp==6):
      print("Exiting the calculator")   
 else:
     print("Invalid input.Enter a valid choice(1/2/3/4/5/6): ")    
-            
+                       
